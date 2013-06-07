0395b91bcbea328d4118136387c89c187db34057
John McMellen <jmcmellen@missouristate.edu>
2013-06-07 15:22:17 -0400

    C9

--- web.js
+++ web.js
@@ -3,7 +3,7 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World! \n What\'s Up?');
+  response.send('Hello World! \n What\'s Up Dog?');
 });
 
 var port = process.env.PORT || 5000;
